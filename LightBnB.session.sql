213a952548bd6f6b565b1dead395915c22bc02b0
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-07-30 09:08:33 -0700

    Refactor the user functions in database.js to retrieve data from the database instead of the in-memory data

---
+++ LightBnB.session.sql
@@ -0,0 +1,5 @@
+--select * from users where name='Rauber';
+--$2b$12$SREwn4X1VTbD7R1rCmFyIuIR8zlVwKjce.qOlJ2CYcMd5rMq4RZSS
+--$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+
+select * from users limit 10;
